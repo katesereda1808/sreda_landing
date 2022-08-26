75748fa5122fb9349dd0eeffd3474424ea7f0bb5
katesereda1808 <katesereda22@gmail.com>
2022-08-26 12:38:29 +0200

    mobile & desktop menu, contacts

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+'use strict';
 const open_menu = document.querySelector('.header__menu_btn');
-const menu = document.querySelector('.header__menu_mobile');
-const close_menu = document.querySelector('.close_menu_btn');
+const all_menu = document.querySelectorAll('.header__menu');
+const all_close_menu = document.querySelectorAll('.close_menu_btn');
 
 
 open_menu.addEventListener('click', ()=>{
-    console.log(menu)
-    menu.classList.remove('hidden')
+    console.log(all_menu)
+    for(let i=0; i<all_menu.length; i++){
+        console.log(all_menu[i]);
+        all_menu[i].classList.remove('hidden');
+    }
 })
-close_menu.addEventListener('click', ()=>{
-    menu.classList.add('hidden')
-})
+for (let i = 0; i < all_close_menu.length; i++) {
+    all_close_menu[i].addEventListener('click',()=>{
+        for(let i=0; i<all_menu.length; i++){
+            console.log(all_menu[i]);
+            all_menu[i].classList.add('hidden');
+        }
+    })
+}
