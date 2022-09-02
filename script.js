311140c89e800f78affcd44e127bfd5cea035cf1
katesereda1808 <katesereda22@gmail.com>
2022-09-02 14:08:38 +0300

    hover effects are added to menud

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 'use strict';
 const open_menu = document.querySelector('.header__menu_btn');
-const all_menu = document.querySelectorAll('.header__menu');
-const all_close_menu = document.querySelectorAll('.close_menu_btn');
+const all_menu = Array.from(document.querySelectorAll('.header__menu'));
+const all_close_menu = Array.from(document.querySelectorAll('.close_menu_btn'));
+const menu_container = Array.from(document.querySelectorAll('.header__menu_container'));
+console.log(menu_container)
+// header__menu_container
 
+menu_container.map(item=>{
+    item.addEventListener('blur',()=>{
+        console.log('blurrrr')
+        all_menu.map(menu=>{
+            menu.classList.add('hidden')
+        })
+    })
+})
 
 open_menu.addEventListener('click', ()=>{
-    console.log(all_menu)
-    for(let i=0; i<all_menu.length; i++){
-        console.log(all_menu[i]);
-        all_menu[i].classList.remove('hidden');
-    }
-})
-for (let i = 0; i < all_close_menu.length; i++) {
-    all_close_menu[i].addEventListener('click',()=>{
-        for(let i=0; i<all_menu.length; i++){
-            console.log(all_menu[i]);
-            all_menu[i].classList.add('hidden');
-        }
+    all_menu.map(item=>{
+        item.classList.remove('hidden')
     })
-}
-
+})
+all_close_menu.map(item=>{
+    item.addEventListener('click',()=>{
+        all_menu.map(item=>{
+            item.classList.add('hidden')
+        })
+    })
+})
 
 const img_gallery = document.querySelector('.gallery_mobile_images');
 const arrow_left = document.querySelector('.arrow_left');
