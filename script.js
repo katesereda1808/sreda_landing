af2e50f16cb1ee1703acf27fb7afd92c88d5dd1a
katesereda1808 <katesereda22@gmail.com>
2022-09-05 09:03:48 +0300

    slider made infinite

--- script.js
+++ script.js
@@ -28,17 +28,49 @@ all_close_menu.map(item=>{
     })
 })
 
-const img_gallery = document.querySelector('.gallery_mobile_images');
-const arrow_left = document.querySelector('.arrow_left');
-const arrow_right = document.querySelector('.arrow_right');
+// const img_gallery = document.querySelector('.gallery_mobile_images');
+// const arrow_left = document.querySelector('.arrow_left');
+// const arrow_right = document.querySelector('.arrow_right');
 
-arrow_left.addEventListener('click', ()=>{
-    console.log('arrow_left')
-  img_gallery.scrollLeft-=300;
-})
-arrow_right.addEventListener('click', ()=>{
-    console.log('arrow_right')
-  img_gallery.scrollLeft+=300;
-})
+// arrow_left.addEventListener('click', ()=>{
+//     console.log('arrow_left')
+//   img_gallery.scrollLeft-=300;
+// })
+// arrow_right.addEventListener('click', ()=>{
+//     console.log('arrow_right')
+//   img_gallery.scrollLeft+=300;
+// })
 
 // arrow_right
+
+
+let carousel = document.querySelector('.gallery_mobile_images');
+let images = document.querySelectorAll('.gallery_mobile_images img');
+let toTheLeft = document.querySelector('.arrow_left');
+let toTheRight = document.querySelector('.arrow_right');
+let counter = 1;
+const size = 495;
+carousel.style.transform = 'translate('+ (-size*counter) + 'px)';
+toTheRight.addEventListener('click',()=>{
+    console.log('ncde')
+    carousel.style.transition = "transform 0.7s linear";
+    counter++;
+    carousel.style.transform = 'translate('+ (-size*counter) + 'px)';
+})
+toTheLeft.addEventListener('click',()=>{
+    carousel.style.transition = "transform 0.7s linear";
+    counter--;
+    carousel.style.transform = 'translate('+ (-size*counter) + 'px)';
+})
+carousel.addEventListener('transitionend',()=>{
+    if(images[counter].id === 'lastClone'){
+        carousel.style.transition = "none";
+        counter = images.length - 2;
+        carousel.style.transform = 'translate('+ (-size*counter) + 'px)';
+    }
+    if(images[counter].id === 'firstClone'){
+        carousel.style.transition = "none";
+        counter = images.length - counter;
+        carousel.style.transform = 'translate('+ (-size*counter) + 'px)';
+    }
+})
